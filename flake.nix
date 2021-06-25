9f6f124704754adc3d788570dd6551a3c8e25187
rpgwaiter <rpgwaiter@based.zone>
2021-06-25 18:51:14 -0500

    added module

--- flake.nix
+++ flake.nix
@@ -27,6 +27,25 @@
         };
       };
       defaultPackage.x86_64-linux = packages.x86_64-linux.basedapi;
-      
+
+      nixosModule.x86_64-linux = { lib, pkgs, config }:
+        with lib;                      
+        let
+          cfg = config.services.basedapi;
+        in {
+          options.services.basedapi = {
+            enable = mkEnableOption "basedapi service";
+          };
+
+          config = mkIf cfg.enable {
+            systemd.services.basedapi = {
+              wantedBy = [ "multi-user.target" ];
+              serviceConfig = {
+                  ExecStart = "${pkgs.basedapi}/bin/basedapi";
+              };
+            };
+          };
+        };
+  
     };
 }
